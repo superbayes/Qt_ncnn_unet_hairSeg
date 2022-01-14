0ff3a78024cad597149bcd544a07d76001891736
juxing <xing_ju@seeya-tech.com>
2022-01-14 15:12:00 +0800

    add vs2019 project

---
+++ vs2019/ncnnModel/ncnnModel.cpp
@@ -0,0 +1,18 @@
+﻿
+#pragma once
+#include <iostream>
+#include"opencv2/opencv.hpp"
+#include "nanodet.h"
+int main()
+{
+    //src的尺寸最好是288*288
+    cv::Mat src = cv::imread("model/girl.jpg",-1);
+    NanoDet g_nanodet;
+    const char* modelName = "model/hair";
+
+    g_nanodet.load(modelName, false);
+    g_nanodet.draw(src);
+
+    std::cout << "Hello World!\n";
+}
+
